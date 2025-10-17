e28b6c9ce84bb7c8c8878388a38a966d65412555
Finn Williams <finnreg47@gmail.com>
2025-10-17 17:37:52 -0400

    update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,9 +3,10 @@
 	<div class="max-w-none p-8 text-left md:max-w-md">
 		<h3 class="font-mono text-2xl">About</h3>
 		<p class="pt-4">
-			I bought get-worms.com on whim and had been thinking about what project to make under it for
-			years. Still thinking about it. <br /> <br />
-			Me (Finn) and my good friend Daniel starting working on the idea at the start of September, 2025.
+			I bought get-worms.com on whim and had been thinking about what project to make with it for
+			years.<br />
+			My good friend Daniel, any myself (Finn), started working on the idea at the start of September,
+			2025, to bring brains worms to all humans.
 		</p>
 		<div class="w-full pt-8 text-center">
 			<a class="hover:underline" href="/">Home</a>
